bf3ec4ccfa7dc69933bec33811b889d08458482f
Bernardo Martinez <bernardo-martinez@utc.edu>
2022-08-29 13:14:39 -0400

    version 1.0, working prototype of 11ty campaign

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,8 @@ const htmlmin = require('html-minifier')
 const UpgradeHelper = require("@11ty/eleventy-upgrade-help");
 const { DateTime } = require("luxon");
 const fs = require("fs");
+require("aos");
+require("blazy");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const pluginSyntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 const pluginNavigation = require("@11ty/eleventy-navigation");
@@ -10,110 +12,110 @@ const markdownItAnchor = require("markdown-it-anchor");
 
 
 module.exports = function(eleventyConfig) {
-  /**
-   * Upgrade helper
-   * Uncomment if you need help upgrading to new major version.
-   */
-  //eleventyConfig.addPlugin(UpgradeHelper);
+    /**
+     * Upgrade helper
+     * Uncomment if you need help upgrading to new major version.
+     */
+    //eleventyConfig.addPlugin(UpgradeHelper);
 
-  /**
-   * Files to copy
-   * https://www.11ty.dev/docs/copy/
-   */
-  eleventyConfig.addPassthroughCopy('src/img')
-   // Add plugins
-   eleventyConfig.addPlugin(pluginRss);
-   eleventyConfig.addPlugin(pluginSyntaxHighlight);
-   eleventyConfig.addPlugin(pluginNavigation);
- 
-   eleventyConfig.addFilter("readableDate", dateObj => {
-     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
-   });
- 
-   // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-   eleventyConfig.addFilter('htmlDateString', (dateObj) => {
-     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
-   });
- 
-   // Get the first `n` elements of a collection.
-   eleventyConfig.addFilter("head", (array, n) => {
-     if(!Array.isArray(array) || array.length === 0) {
-       return [];
-     }
-     if( n < 0 ) {
-       return array.slice(n);
-     }
- 
-     return array.slice(0, n);
-   });
- 
-   // Return the smallest number argument
-   eleventyConfig.addFilter("min", (...numbers) => {
-     return Math.min.apply(null, numbers);
-   });
- 
-   function filterTagList(tags) {
-     return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
-   }
- 
-   eleventyConfig.addFilter("filterTagList", filterTagList)
- 
-   // Create an array of all tags
-   eleventyConfig.addCollection("tagList", function(collection) {
-     let tagSet = new Set();
-     collection.getAll().forEach(item => {
-       (item.data.tags || []).forEach(tag => tagSet.add(tag));
-     });
- 
-     return filterTagList([...tagSet]);
-   });
- 
-   // Customize Markdown library and settings:
-   let markdownLibrary = markdownIt({
-     html: true,
-     breaks: true,
-     linkify: true
-   }).use(markdownItAnchor, {
-     permalink: markdownItAnchor.permalink.ariaHidden({
-       placement: "after",
-       class: "direct-link",
-       symbol: "#",
-       level: [1,2,3,4],
-     }),
-     slugify: eleventyConfig.getFilter("slug")
-   });
-   eleventyConfig.setLibrary("md", markdownLibrary);
+    /**
+     * Files to copy
+     * https://www.11ty.dev/docs/copy/
+     */
+    eleventyConfig.addPassthroughCopy('src/img')
+        // Add plugins
+    eleventyConfig.addPlugin(pluginRss);
+    eleventyConfig.addPlugin(pluginSyntaxHighlight);
+    eleventyConfig.addPlugin(pluginNavigation);
 
+    eleventyConfig.addFilter("readableDate", dateObj => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat("dd LLL yyyy");
+    });
 
+    // https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
+    eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toFormat('yyyy-LL-dd');
+    });
 
+    // Get the first `n` elements of a collection.
+    eleventyConfig.addFilter("head", (array, n) => {
+        if (!Array.isArray(array) || array.length === 0) {
+            return [];
+        }
+        if (n < 0) {
+            return array.slice(n);
+        }
 
+        return array.slice(0, n);
+    });
 
+    // Return the smallest number argument
+    eleventyConfig.addFilter("min", (...numbers) => {
+        return Math.min.apply(null, numbers);
+    });
 
-
-  /**
-   * HTML Minifier for production builds
-   */
-  eleventyConfig.addTransform('htmlmin', function (content, outputPath) {
-    if (
-      process.env.ELEVENTY_ENV == 'production' &&
-      outputPath &&
-      outputPath.endsWith('.html')
-    ) {
-      let minified = htmlmin.minify(content, {
-        useShortDoctype: true,
-        removeComments: true,
-        collapseWhitespace: true,
-      })
-      return minified
+    function filterTagList(tags) {
+        return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
     }
 
-    return content
-  })
+    eleventyConfig.addFilter("filterTagList", filterTagList)
 
-  return {
-    dir: {
-      input: "src",
-      data: "../_data"
-    }
-  };
+    // Create an array of all tags
+    eleventyConfig.addCollection("tagList", function(collection) {
+        let tagSet = new Set();
+        collection.getAll().forEach(item => {
+            (item.data.tags || []).forEach(tag => tagSet.add(tag));
+        });
+
+        return filterTagList([...tagSet]);
+    });
+
+    // Customize Markdown library and settings:
+    let markdownLibrary = markdownIt({
+        html: true,
+        breaks: true,
+        linkify: true
+    }).use(markdownItAnchor, {
+        permalink: markdownItAnchor.permalink.ariaHidden({
+            placement: "after",
+            class: "direct-link",
+            symbol: "#",
+            level: [1, 2, 3, 4],
+        }),
+        slugify: eleventyConfig.getFilter("slug")
+    });
+    eleventyConfig.setLibrary("md", markdownLibrary);
+
+
+
+
+
+
+
+    /**
+     * HTML Minifier for production builds
+     */
+    eleventyConfig.addTransform('htmlmin', function(content, outputPath) {
+        if (
+            process.env.ELEVENTY_ENV == 'production' &&
+            outputPath &&
+            outputPath.endsWith('.html')
+        ) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true,
+            })
+            return minified
+        }
+
+        return content
+    })
+
+    return {
+        dir: {
+            input: "src",
+            data: "../_data"
+        }
+    };
 };
