5b3995cd71c32e1f750b5e64a9c8fe953851bd0d
Bernardo Martinez <bernardo-martinez@utc.edu>
2022-08-31 13:01:27 -0400

    version 2.3, comment back to the top manually set background white for menu links.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -80,19 +80,19 @@ function throwConfetti() { // start
 
 
 //   <!--Back to the top-->
-const showOnPx = 100;
-const backToTopButton = document.querySelector(".back-to-top");
-const scrollContainer = () => {
-    return document.documentElement || document.body;
-};
-const goToTop = () => {
-    document.body.scrollIntoView({ behavior: "smooth" });
-};
-document.addEventListener("scroll", () => {
-    if (scrollContainer().scrollTop > showOnPx) {
-        backToTopButton.classList.remove("hidden");
-    } else {
-        backToTopButton.classList.add("hidden");
-    }
-});
-backToTopButton.addEventListener("click", goToTop);
+// const showOnPx = 100;
+// const backToTopButton = document.querySelector(".back-to-top");
+// const scrollContainer = () => {
+//     return document.documentElement || document.body;
+// };
+// const goToTop = () => {
+//     document.body.scrollIntoView({ behavior: "smooth" });
+// };
+// document.addEventListener("scroll", () => {
+//     if (scrollContainer().scrollTop > showOnPx) {
+//         backToTopButton.classList.remove("hidden");
+//     } else {
+//         backToTopButton.classList.add("hidden");
+//     }
+// });
+// backToTopButton.addEventListener("click", goToTop);
